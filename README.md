ca2852b4dc3d6b0d3401a3b3504799dce3ccc04d
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-06-07 10:56:01 +0200

    inital configuration
    initial configuration for Sony Xperia X (suzu)
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# device-sony-suzu
+Copyright (C) Sony Mobile Communications 2016
+=============================================
+
+This is the Android device configuration for Xperia X(loire platform).
+
+Build instructions
+http://developer.sonymobile.com/knowledge-base/open-source/open-devices/aosp-build-instructions/
---
+++ device.mk
@@ -0,0 +1,63 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DEVICE_PACKAGE_OVERLAYS += \
+    device/sony/suzu/overlay
+
+PRODUCT_COPY_FILES := \
+    device/sony/suzu/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
+    device/sony/suzu/rootdir/system/etc/BCM43455.hcd:system/etc/firmware/BCM43xx.hcd \
+    device/sony/suzu/rootdir/system/etc/wifi/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
+    device/sony/suzu/rootdir/system/etc/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf \
+    device/sony/suzu/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    device/sony/suzu/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
+    device/sony/suzu/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml
+
+# Device Specific Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml
+
+# Device Init
+PRODUCT_PACKAGES += \
+    init.recovery.suzu \
+    init.suzu \
+    ueventd.suzu
+
+# Lights
+PRODUCT_PACKAGES += \
+    lights.suzu
+
+# Simple PowerHAL
+PRODUCT_PACKAGES += \
+    power.suzu
+
+# NFC config
+PRODUCT_PACKAGES += \
+    nfc_nci.suzu
+
+PRODUCT_AAPT_CONFIG := large
+PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.sf.lcd_density=480 \
+    ro.usb.pid_suffix=1DB
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/sony/loire/platform.mk)
+$(call inherit-product, vendor/sony/loire-suzu/suzu-vendor.mk)
