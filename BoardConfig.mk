d10969f3917fb8167a4e5b81b5b36139e9dc2c42
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-09-14 15:37:52 +0200

    declare PRODUCT_PLATFORM
    PRODUCT_PLATFORM is used as build barier on all projects and
    should be defined for each device
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,6 +16,9 @@ include device/sony/loire/PlatformConfig.mk
 
 TARGET_BOOTLOADER_BOARD_NAME := F5121
 
+# Platform
+PRODUCT_PLATFORM := loire
+
 BOARD_KERNEL_CMDLINE += androidboot.hardware=suzu
 
 # Recovery config
